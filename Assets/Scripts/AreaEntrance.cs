e91008a11054e5fd0854918292e79569226d1cf7
Tunaanhgamedev <161286441+Tunaanhgamedev@users.noreply.github.com>
2026-04-21 00:16:41 +0700

    area

--- Assets/Scripts/AreaEntrance.cs
+++ Assets/Scripts/AreaEntrance.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class AreaEntrance : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private string transitionName;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        if(transitionName == SceneManagement.Instance.SceneTransitionName)
+        {
+            PlayerController.Instance.transform.position = this.transform.position;
+        }
     }
 }
--- Assets/Scripts/Singleton.cs
+++ Assets/Scripts/Singleton.cs
@@ -2,17 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Singleton : MonoBehaviour
+public class Singleton<T> : MonoBehaviour where T : Singleton<T>
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private static T instance;
+    public static T Instance { get { return instance; } }
 
-    // Update is called once per frame
-    void Update()
+    protected virtual void Awake()
     {
-        
+        if (instance != null && this.gameObject != null)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = (T)this;
+        } 
+        DontDestroyOnLoad(gameObject);    
     }
 }
