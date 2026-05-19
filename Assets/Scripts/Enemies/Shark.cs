1bed69b69edd22c51c555bfaf6d2b3c3f679bbb2
Tunaanhgamedev <161286441+Tunaanhgamedev@users.noreply.github.com>
2026-05-20 01:44:06 +0700

    done Shark

--- Assets/Scripts/Enemies/Shark.cs
+++ Assets/Scripts/Enemies/Shark.cs
@@ -2,17 +2,62 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Shark : MonoBehaviour
+public class Shark : MonoBehaviour, IEnemy
 {
-    // Start is called before the first frame update
-    void Start()
+    private Animator myAnimator;
+    private SpriteRenderer spriteRenderer;
+
+    readonly int ATTACK_HASH = Animator.StringToHash("attacking");
+    readonly int DIE_HASH = Animator.StringToHash("die");
+
+    private bool isDead = false;
+
+    private void Awake()
     {
-        
+        myAnimator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Attack()
     {
-        
+        if (isDead) return;
+
+        myAnimator.SetTrigger(ATTACK_HASH);
+
+        if (transform.position.x < PlayerController.Instance.transform.position.x)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else
+        {
+            spriteRenderer.flipX = true;
+        }
+    }
+
+    public void Die()
+    {
+        if (isDead) return;
+
+        isDead = true;
+
+        myAnimator.SetTrigger(DIE_HASH);
+
+        GetComponent<Collider2D>().enabled = false;
+        GetComponent<SharkChase>().enabled = false;
+        GetComponent<EnemyPathfinding>().StopMoving();
+
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+
+        if (rb != null)
+        {
+            rb.velocity = Vector2.zero;
+        }
+
+        Destroy(gameObject, 1.5f);
+    }
+
+    public void DamagePlayerAnimEvent()
+    {
+        PlayerHealth.Instance.TakeDamage(1, transform);
     }
 }
---
+++ Assets/Scripts/Enemies/SharkChase.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+
+public class SharkChase : MonoBehaviour
+{
+    [Header("Chase")]
+    [SerializeField] private float chaseRange = 10f;
+    [SerializeField] private float stopDistance = .5f;
+    [SerializeField] private float attackCooldown = 1f;
+
+    [Header("Roam")]
+    [SerializeField] private float roamChangeDirTime = 2f;
+
+    private EnemyPathfinding pathfinding;
+    private Shark shark;
+
+    private float attackTimer;
+
+    private Vector2 roamDirection;
+    private float roamTimer;
+
+    private void Awake()
+    {
+        pathfinding = GetComponent<EnemyPathfinding>();
+        shark = GetComponent<Shark>();
+    }
+
+    private void Start()
+    {
+        ChooseRoamDirection();
+    }
+
+    private void Update()
+    {
+        attackTimer -= Time.deltaTime;
+        roamTimer += Time.deltaTime;
+
+        Vector2 playerPos = PlayerController.Instance.transform.position;
+        Vector2 myPos = transform.position;
+
+        float distance = Vector2.Distance(myPos, playerPos);
+
+        // =========================
+        // CHASE PLAYER
+        // =========================
+        if (distance <= chaseRange)
+        {
+            // Di chuyển tới player
+            if (distance > stopDistance)
+            {
+                Vector2 direction = (playerPos - myPos).normalized;
+
+                pathfinding.MoveTo(direction);
+            }
+            else
+            {
+                // Attack
+                pathfinding.StopMoving();
+
+                if (attackTimer <= 0)
+                {
+                    attackTimer = attackCooldown;
+
+                    shark.Attack();
+                }
+            }
+        }
+
+        // =========================
+        // RANDOM ROAM
+        // =========================
+        else
+        {
+            pathfinding.MoveTo(roamDirection);
+
+            if (roamTimer >= roamChangeDirTime)
+            {
+                ChooseRoamDirection();
+            }
+        }
+    }
+
+    private void ChooseRoamDirection()
+    {
+        roamTimer = 0f;
+
+        roamDirection = new Vector2(
+            Random.Range(-1f, 1f),
+            Random.Range(-1f, 1f)
+        ).normalized;
+    }
+}
