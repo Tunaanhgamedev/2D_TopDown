f1d74ee67359eab092f2b73b68cb4847afa7810c
Tunaanhgamedev <shelockhomles2005@gmail.com>
2026-08-05 13:21:26 +0700

    feat: implement Monk class structure and improve attack/animation logic

--- Assets/Scripts/Enemies/Monk/Monk.cs
+++ Assets/Scripts/Enemies/Monk/Monk.cs
@@ -4,6 +4,59 @@ using UnityEngine;
 
 public class Monk : MonoBehaviour
 {
-    
+    private Animator myAnimator;
+    private SpriteRenderer spriteRenderer;
 
+    readonly int ATTACK_HASH = Animator.StringToHash("attacking");
+    readonly int DIE_HASH = Animator.StringToHash("die");
+
+    private bool isDead = false;
+
+    private void Awake()
+    {
+        myAnimator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void Attack()
+    {
+        if (isDead) return;
+        myAnimator.SetTrigger(ATTACK_HASH);
+        if (transform.position.x < PlayerController.Instance.transform.position.x)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else
+        {
+            spriteRenderer.flipX = true;
+        }
+    }
+
+    public void Die()
+    {
+        if (isDead) return;
+
+        isDead = true;
+        myAnimator.SetTrigger(DIE_HASH);
+        GetComponent<Collider2D>().enabled = false;
+        MonkChase monkChase = GetComponent<MonkChase>();
+        if (monkChase != null)
+        {
+            monkChase.enabled = false;
+        }
+        GetComponent<EnemyPathfinding>().StopMoving();
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+
+        if (rb != null)
+        {
+            rb.velocity = Vector2.zero;
+        }
+
+        Destroy(gameObject, 1.5f);
+    }
+
+    public void DamagePlayerAnimEvent()
+    {
+        PlayerHealth.Instance.TakeDamage(1, transform);
+    }
 }
